38d1d7ec01f4137c8b55ca4a889fe7a1d2ef5ba3
Liellison Menezes <liellisonjoseph@gmail.com>
2024-02-28 10:55:03 -0300

    Init

--- LilNavs/ContentView.swift
+++ LilNavs/ContentView.swift
@@ -6,85 +6,50 @@
 //
 
 import SwiftUI
-import CoreData
+import WebKit
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var showWebView = false
+    @State private var urlString: String = "https://www.google.com"
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        VStack(spacing: 10) {
+            WebView(url: URL(string: urlString)!)
+                .cornerRadius(10)
+                .shadow(color: .black.opacity(0.3), radius: 20.0, x: 5, y: 5)
+            TextField("Enter Web Address",text: $urlString)
+                .foregroundColor(.white)
+                .padding(8)
+                .background(Color.gray)
+                .cornerRadius(25)
+                .controlSize(.small)
+                .keyboardType(.URL)
+//            HStack{
+//                Button(action: {
+//                    //webViewStateModel.pageTitle = urlString
+//                    //webViewStateModel.goToPage = true
+//                }, label: {
+//                    Image(systemName: "greaterthan.circle.fill")
+//                })
+//                .font(.title)
+//            }
+        }.padding()
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct WebView: UIViewRepresentable {
+    var url: URL
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
